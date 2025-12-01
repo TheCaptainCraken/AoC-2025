9cec7935637fbe2bdba5898b8faa518979749734
Pietro Agnoli <pietro_agnoli@proton.me>
2025-12-01 12:55:12 +0100

    first commit

---
+++ rust/day01/src/lib.rs
@@ -0,0 +1,70 @@
+const DIAL_START_POSITION: i32 = 50;
+const MODULO_DIAL: i32 = 100;
+
+pub fn part1(input: &str) -> String {
+    input
+        .lines()
+        .fold((0, DIAL_START_POSITION), |(zeroes, dial_position), line| {
+            let number: i32 = line.chars().skip(1).collect::<String>().parse().unwrap();
+            let mut new_dial_position = dial_position;
+            if line.chars().nth(0).unwrap() == 'L' {
+                new_dial_position -= number;
+            } else {
+                new_dial_position += number;
+            }
+
+            new_dial_position %= MODULO_DIAL;
+
+            if new_dial_position == 0 {
+                (zeroes + 1, new_dial_position)
+            } else {
+                (zeroes, new_dial_position)
+            }
+        })
+        .0
+        .to_string()
+}
+
+pub fn part2(input: &str) -> String {
+    input
+        .lines()
+        .fold((0, DIAL_START_POSITION), |(zeros, dial_position), line| {
+            let number: i32 = line.chars().skip(1).collect::<String>().parse().unwrap();
+            let mut new_dial_position = dial_position;
+            if line.chars().nth(0).unwrap() == 'L' {
+                new_dial_position -= number;
+            } else {
+                new_dial_position += number;
+            }
+
+            let count_position = if new_dial_position < 0 {
+                new_dial_position.abs() + 98
+            } else {
+                new_dial_position
+            };
+
+            (
+                zeros + count_position / MODULO_DIAL,
+                new_dial_position % MODULO_DIAL,
+            )
+        })
+        .0
+        .to_string()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part1() {
+        let input = include_str!("../test_input.txt");
+        assert_eq!(part1(input), "3");
+    }
+
+    #[test]
+    fn test_part2() {
+        let input = include_str!("../test_input.txt");
+        assert_eq!(part2(input), "6");
+    }
+}
---
+++ rust/day01/src/main.rs
@@ -0,0 +1,8 @@
+use day01::{part1, part2};
+
+fn main() {
+    let input = include_str!("../../../inputs/day01.txt");
+
+    println!("Part 1: {}", part1(input));
+    println!("Part 2: {}", part2(input));
+}
